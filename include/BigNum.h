12affafbe9a08b9c4bfea00356d22d06a4658625
Matt Traudt <matt@mineoas.us>
2014-09-08 22:25:21 -0500

    Added some equality operators and some "optimizations" to addition

--- include/BigNum.h
+++ include/BigNum.h
@@ -9,14 +9,21 @@
 class BigNum
 {
     public:
-        std::string get() {return m_value;}
+        std::string get();
         BigNum operator+(BigNum that);
+        bool operator==(BigNum that)      {return this->get() == that.get();}
+        bool operator!=(BigNum that)      {return this->get() != that.get();}
+        bool operator==(std::string that) {return this->get() == that;}
+        bool operator!=(std::string that) {return this->get() == that;}
+        bool operator==(std::vector<char> that) {return this->m_value == that;}
+        bool operator!=(std::vector<char> that) {return this->m_value != that;}
         BigNum();
         BigNum(std::string val);
+        BigNum(std::vector<char> val);
         virtual ~BigNum();
     protected:
     private:
-        std::string m_value;
+        std::vector<char> m_value;
 };
 
 #endif // BIGNUM_H
--- src/BigNum.cpp
+++ src/BigNum.cpp
@@ -3,12 +3,22 @@
 int8_t char2digit(char c) {return c-'0';}
 char digit2char(int8_t i) {return i+'0';}
 
+void flip(std::vector<char>* val)
+{
+    char buf;
+    for (unsigned int i = 0; i < val->size() / 2; i++)
+    {
+        buf = val->operator[](i);
+        val->operator[](i) = val->operator[](val->size()-1-i);
+        val->operator[](val->size()-1-i) = buf;
+    }
+}
+
 BigNum BigNum::operator+(BigNum that)
 {
     const auto maxdigits = 1+(this->get().length() > that.get().length() ? this->get().length() : that.get().length());
     std::vector<char> arr1(maxdigits, '0');
     std::vector<char> arr2(maxdigits, '0');
-    std::string ans = "";
     for (unsigned int i = 0; i < this->get().length(); i++)
         arr1[this->get().length()-1-i] = this->get()[i];
     for (unsigned int i = 0; i < that.get().length(); i++)
@@ -20,18 +30,35 @@ BigNum BigNum::operator+(BigNum that)
         carry = 0;
         while (sum >= 10)
             {carry++; sum -= 10;}
-        ans = digit2char(sum) + ans;
+        arr1[i] = digit2char(sum);
     }
-    while (ans.length() > 1 && ans[0] == '0') ans = ans.substr(1, std::string::npos);
-    return BigNum(ans);
+    while (arr1.size() > 1 && arr1[arr1.size()-1] == '0') arr1.pop_back();
+    flip(&arr1);
+    return BigNum(arr1);
+}
+
+std::string BigNum::get()
+{
+    std::string val;
+    for (unsigned int i = 0; i < m_value.size(); i++)
+        val += m_value[i];
+    return val;
+
 }
 
 BigNum::BigNum()
 {
-    m_value = "0";
+    m_value = std::vector<char>('0');
 }
 
 BigNum::BigNum(std::string val)
+{
+    m_value = std::vector<char>(val.length());
+    for (unsigned int i = 0; i < val.length(); i++)
+        m_value[i] = val[i];
+}
+
+BigNum::BigNum(std::vector<char> val)
 {
     m_value = val;
 }
